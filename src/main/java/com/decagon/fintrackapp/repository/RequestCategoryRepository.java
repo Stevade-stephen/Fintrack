52635a2c0ec84ac5be426457cec60553077ef6fa
decagon <remigius.mgbekwute@decagon.dev>
2021-04-10 17:38:03 +0100

    fix bug

--- src/main/java/com/decagon/fintrackapp/repository/RequestCategoryRepository.java
+++ src/main/java/com/decagon/fintrackapp/repository/RequestCategoryRepository.java
@@ -3,7 +3,9 @@ package com.decagon.fintrackapp.repository;
 import com.decagon.fintrackapp.model.RequestCategory;
 import com.decagon.fintrackapp.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RequestCategoryRepository extends JpaRepository<RequestCategory, Long> {
     Boolean existsByName(String name);
 }
--- src/main/java/com/decagon/fintrackapp/repository/TransactionTypeRepository.java
+++ src/main/java/com/decagon/fintrackapp/repository/TransactionTypeRepository.java
@@ -2,7 +2,9 @@ package com.decagon.fintrackapp.repository;
 
 import com.decagon.fintrackapp.model.TransactionType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionTypeRepository extends JpaRepository<TransactionType, Long> {
 
 }
